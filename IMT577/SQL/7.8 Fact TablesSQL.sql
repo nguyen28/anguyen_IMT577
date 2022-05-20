b2894c6c752c71f64780fc914fb008b6c0a042f0
Ashli <njnguyen28@gmail.com>
2022-05-20 02:02:15 -0700

    All three fact tables

--- IMT577/SQL/7.8 Fact TablesSQL.sql
+++ IMT577/SQL/7.8 Fact TablesSQL.sql
@@ -3,45 +3,44 @@
 USE schema IMT577_DW_Ashli_nguyen.Public;
 create table IMT577_DW_Ashli_nguyen.public.Fact_SRCSalesTarget(
     DimStoreID INTEGER CONSTRAINT FK_DimStoreID FOREIGN KEY REFERENCES Dim_Store(DimStoreID) --Foreign Key
-     ,DimResellerID INTEGER CONSTRAINT FK_DimResellerID FOREIGN KEY REFERENCES Dim_Reseller(DimResellerID) --Foreign Key
+    ,DimResellerID INTEGER CONSTRAINT FK_DimResellerID FOREIGN KEY REFERENCES Dim_Reseller(DimResellerID) --Foreign Key
     ,DimChannelID INTEGER CONSTRAINT FK_DimChannelID FOREIGN KEY REFERENCES Dim_Channel(DimChannelID) --Foreign Key
-     ,DimLocationID INTEGER CONSTRAINT FK_DimLocationID FOREIGN KEY REFERENCES Dim_Location(DimLocationID) --Foreign Key
-     ,SourceSalesHeaderID INT --Natural Key
-    ,SourceSalesDetailID INTEGER --Natural Key 
-	,SaleAmount FLOAT
-    ,SaleQuantity INT
-
-
+    ,DimTargetDateID number(9) CONSTRAINT FK_DimTargetDateID FOREIGN KEY REFERENCES Dim_Date(DATE_PKEY) --Foreign Key,SourceSalesHeaderID INT --Natural Key
+    ,SalesTargetAmount Float 
 );
 
-INSERT INTO Fact_ProductSaleTarget(
-    Dim_Product.DimProductID 
-     ,Dim_Store.DimStoreID 
-     ,Dim_reseller.DimResellerID 
-     ,Dim_Customer.DimCustomerID 
-     ,Dim_Channel.DimChannelID 
-     ,Dim_Date.DimSalesDateID 
-     ,Dim_Location.DimLocationID 
-     ,SourceSalesHeaderID 
-    ,SourceSalesDetailID 
-	,SaleAmount 
-    ,SaleQuantity 
-    ,SaleUnitPrice 
-    ,SaleExtendedCost 
-    ,SaleTotalProfit 
+INSERT INTO Fact_ProductSaleTarget( 
+    DimStoreID 
+    ,DimResellerID 
+    ,DimChannelID 
+    ,DimTargetDateID
+    ,SalesTargetAmount 
 )
-FROM 
+    
+    Dim_Store.DimStoreID
+    ,Dim_Reseller.DimResellerID
+    ,Dim_Channel.DimChannelID 
+    ,Dim_Date.Date_PKey AS DimTargetDateID
+    ,Targetdata_Channel.Targetsalesamount AS SalesTargetAmount
+
+FROM Dim_Channel
+    INNER JOIN Targetdata_Channel ON DIM_Channel.Channel= Targetdata_Channel.ChannelName
+    Inner Join SalesHeader ON DIM_Channel.DimChannelID= SalesHeader.ChannelID 
+    Left Outer Join DIM_Reseller ON SalesHeader.ResellerID = DIM_Reseller.DIMResellerID
+    Left Outer Join Dim_Store ON SalesHeader.StoreID = DIM_Store.DimStoreID
+    LEFT OUTER Join DIM_Date ON Targetdata_Channel.Year = DIM_Date.Year
+
+
 
 --Create Fact_SalesActual
 USE schema IMT577_DW_Ashli_nguyen.Public;
 create table IMT577_DW_Ashli_nguyen.public.Fact_SalesActual (
-(
-    DimProductID INT CONSTRAINT FK_DimProductID FOREIGN KEY REFERENCES Dim_Product(DimProductID) --Foreign Key
+    DimProductID INTEGER CONSTRAINT FK_DimProductID FOREIGN KEY REFERENCES Dim_Product(DimProductID) --Foreign Key
     ,DimStoreID INTEGER CONSTRAINT FK_DimStoreID FOREIGN KEY REFERENCES Dim_Store(DimStoreID) --Foreign Key
     ,DimResellerID INTEGER CONSTRAINT FK_DimResellerID FOREIGN KEY REFERENCES Dim_Reseller(DimResellerID) --Foreign Key
     ,DimCustomerID INTEGER CONSTRAINT FK_DimCustomerID FOREIGN KEY REFERENCES Dim_Customer(DimCustomerID) --Foreign Key
     ,DimChannelID INTEGER CONSTRAINT FK_DimChannelID FOREIGN KEY REFERENCES Dim_Channel(DimChannelID) --Foreign Key
-    ,DimSalesDateID INTEGER CONSTRAINT FK_DimSalesDateID FOREIGN KEY REFERENCES Dim_Date(DATE_PKEY) --Foreign Key
+    ,DimSalesDateID number(9) CONSTRAINT FK_DimSalesDateID FOREIGN KEY REFERENCES Dim_Date(DATE_PKEY) --Foreign Key
     ,DimLocationID INTEGER CONSTRAINT FK_DimLocationID FOREIGN KEY REFERENCES Dim_Location(DimLocationID) --Foreign Key
     ,SourceSalesHeaderID INT
     ,SourceSalesDetailID INT
@@ -90,7 +89,7 @@ FROM
     Left Outer Join DIM_Reseller ON SalesHeader.ResellerID = DIM_Channel.DIMResellerID
     Left Outer Join DIM_Store ON SalesHeader.StoreID = DIM_Channel.DimStoreID
     Left Outer Join DIM_Customer ON SalesHeader.CustomerID = DIM_Customer.DimCustomerID
-    LEFT OUTER Join DIM_Date ON .Year = DIM_Date.Year
+    LEFT OUTER Join DIM_Date ON Salesheader.date = DIM_Date.Date_Pkey
 
 
 --create Fact_ProductSaleTarget
